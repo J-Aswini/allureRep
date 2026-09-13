8ba13d73a91a7600b30cea13abc34ab5e7ed860d
J-Aswini <aswini.jan2002@gmail.com>
2026-09-13 17:30:14 +0530

    "update config"

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,9 +23,10 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
 reporter: [
-['html'],
-['list'],
-['allure-playwright']
+  ['list'],
+    ['allure-playwright', {
+	resultsDir: 'allure-results'
+	}]
 ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
